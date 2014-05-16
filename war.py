ebdcaeafc44eb791fab7ce3e1d33b0457cfa5f91
flytape8490 <rivard8490@gmail.com>
2014-05-16 18:12:28 -0500

    stat
    Optimized. set it up to run multiple times and return an average game
    length.

--- war.py
+++ war.py
@@ -1,6 +1,6 @@
 # Python 3.x
 # War -- war.py
-# v0.15
+# v0.16
 
 # A python replication of the War cardgame. It's also a learning 
 # exercise for me to learn how object classes work.
@@ -10,39 +10,38 @@ class deck:
 	def __init__(self,pool):
 		self.pack=pool
 	def draw(self):
-		return self.pack.pop(0)
-		#add a try to figure out how to break when cards run out during war. Maybe return 'None' and add a ' if none in pack' statement?
+		try: return self.pack.pop(0)
+		except IndexError: return None
 	def reset(self):
 		self.pack=[]
 	def take(self,pool,shuf=True):
 		if shuf==True:shuffle(pool)
-		for i in pool:
-			self.pack.append(i)
-			
-def war(): #[a1,b1,adown,bdown,aup,bup]  #-2=aup, -1=bup
-	pot.take([a.draw(),b.draw(),a.draw(),b.draw()],False)
-	if pot.pack[-2]>pot.pack[-1]:	#aup>bup
-		for i in pot.pack:
-			a.take(pot.pack)
-	elif pot.pack[-2]<pot.pack[-1]:
-			b.take(pot.pack)
-	else:
-		war()
-
+		self.pack+=pool
+def war():
+	pot.take([a.draw(),b.draw(),a.draw(),b.draw()],False)#a and b play 1 facedown card, then 1 faceup card
+	if None not in pot.pack: compare()
+def compare():
+	if pot.pack[-2]>pot.pack[-1]: #if a>b...
+		a.take(pot.pack)		#a gets the pot
+	elif pot.pack[-2]<pot.pack[-1]:	#if a<b...
+		b.take(pot.pack)			#b gets the pot
+	else: war()	#else (a==b), war
+	pot.reset()
 def run():
 	turn=0
 	while len(a.pack) and len(b.pack):
 		pot.take([a.draw(),b.draw()],False)
-		if pot.pack[0]>pot.pack[1]: a.take(pot.pack)
-		elif pot.pack[0]<pot.pack[1]: b.take(pot.pack)
-		else: war()
-		pot.reset()
+		compare()
 		turn+=1
 	print(turn)
-
-cards=[i for i in range(2,15)]*4
-shuffle(cards)
-a=deck(cards[:26])
-b=deck(cards[26:])
-pot=deck([])
-run()
+	return turn
+cards=[i for i in range(13)]*4
+ttl=0
+for i in range(999):
+	shuffle(cards)
+	a=deck(cards[:26])
+	b=deck(cards[26:])
+	pot=deck([])
+	ttl+=run()
+print(ttl/(i+1))
+	
